46fedae8de0b0c31db195e012b90edfc3665e7ad
sen5q <95946173+sen5q@users.noreply.github.com>
2023-01-02 14:04:29 +0900

    メソッド化

--- light.py
+++ light.py
@@ -5,7 +5,6 @@ import id
 import switchbot_sign
 import execute_scene
 
-
 # 引数エラーメッセージ
 error_arg_message = "Argument Error: Should Enter Argument\n" \
                     "    python light.py 0    <- Off\n" \
@@ -13,56 +12,71 @@ error_arg_message = "Argument Error: Should Enter Argument\n" \
                     "    python light.py d    <- On(Dark)\n" \
                     "    python light.py n    <- On(Nightlight)\n" \
 
-# ヘッダー作成
-device_id = id.light
-headers = switchbot_sign.make_header(id.token, id.secret)
-url = "https://api.switch-bot.com/v1.1/devices/" + device_id + "/commands"
 
 
+def main():
 
-try:
-    mode = sys.argv[1]
-except:
-    print(error_arg_message)
-    exit()
+    device_id = id.light
+    headers = switchbot_sign.make_header(id.token, id.secret)
+    url = "https://api.switch-bot.com/v1.1/devices/" + device_id + "/commands"
+
+    try:
+        mode = sys.argv[1]
+    except:
+        print(error_arg_message)
+        exit()
+
+    body = make_body(mode)
+
+    send_requests(url, headers, body)
 
 
 
 # 要求データ作成
 # 0=OFF  1=ON(明)  d=ON(暗)  n=常夜灯
+def make_body(mode):
 
-if mode == "0":
-    body={
-    "command": "turnOff",
-    "parameter": "default",
-    "commandType": "command"
-    }
-
-elif mode == "1":
-    body={
-        "command": "turnOn",
+    if mode == "0":     # OFF
+        body={
+        "command": "turnOff",
         "parameter": "default",
         "commandType": "command"
         }
 
-elif mode == "d":
-    execute_scene.main(id.scene_darklight)
-    exit()
+    elif mode == "1":   # ON(明)
+        body={
+            "command": "turnOn",
+            "parameter": "default",
+            "commandType": "command"
+            }
 
-elif mode ==  "n":
-    execute_scene.main(id.scene_nightlight)
-    exit()
+    elif mode == "d":   # ON(暗)
+        execute_scene.main(id.scene_darklight)
+        exit()
 
-else:
-    print(error_arg_message)
-    exit()
+    elif mode ==  "n":  # 常夜鄧
+        execute_scene.main(id.scene_nightlight)
+        exit()
+
+    else:
+        print(error_arg_message)
+        exit()
+
+    return(body)
 
 
 
-# POST
-r = requests.post(url, headers=headers, data=json.dumps(body))
+# bodyを送って結果を表示
+def send_requests(url, headers, body): 
+    # POST
+    r = requests.post(url, headers=headers, data=json.dumps(body))
 
-# 出力内容整形
-json_data = r.json() 
-s = json.dumps(json_data, indent=2, ensure_ascii=False)
-print(s)
+    # 出力内容整形
+    json_data = r.json() 
+    s = json.dumps(json_data, indent=2, ensure_ascii=False)
+    print(s)
+
+
+
+if __name__ == "__main__":
+    main()
