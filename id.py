967f9a4a1002419b7d6ddff6d4bb36c5c1f7d89c
sen5q <95946173+sen5q@users.noreply.github.com>
2023-01-02 14:44:58 +0900

    シーンIDを追加

--- id.py
+++ id.py
@@ -3,3 +3,6 @@ secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 airconditioner = "xx-xxxxxxxxxxxx-xxxxxxxx"
 light = "xx-xxxxxxxxxxxx-xxxxxxxx"
+
+scene_darklight = "xxx-xxxxxxxxxxxx-xxxxxxxx"
+scene_nightlight = "xxx-xxxxxxxxxxxx-xxxxxxxx"
